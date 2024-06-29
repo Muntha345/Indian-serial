866ae3e9ee70df44c762a8c9bd36ff664637ee56
Muntha345 <redoanhossain20167@gmail.com>
2024-06-30 01:10:00 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://dev-ind-serial.pantheonsite.io/` + encodeURI(path as string)
 				}`,
 			},
 		};
